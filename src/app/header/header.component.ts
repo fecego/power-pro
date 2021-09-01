9819432b221e9c2c8f9befa128181451f3a1e1e8
fecego.adrian <fecego.git@gmail.com>
2021-09-01 11:41:38 -0500

    menu casi al 100%, segundo commit se añadio el expand de todas las vistas del menu categorias y subs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +7,217 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild("menuCategoria") categoriaM:ElementRef;
+  @ViewChild("menuEspecialidades") especialidadesM: ElementRef;
+  @ViewChild("menuUbicacion") ubicacionM: ElementRef;
+  @ViewChild("menuOtros") otrosM: ElementRef;
+  @ViewChild("primerVistaMenu") primerVista: ElementRef;
+  @ViewChild("opcionesCategorias") opcCategorias:ElementRef;
+  @ViewChild("opcionesUbicaciones") opcUbicacion:ElementRef;
+  @ViewChild("opcionesEspecialidades") opcEspecialidad: ElementRef;
+  @ViewChild("contenedorPrueba") contenedorP: ElementRef;
+
+
   public categoriaEquipos = false;
   public categoriaHerramientas = false;
   public categoriaMotores = false;
   public categoriaAccesorios = false;
 
-  constructor() { }
+  /*==============================================*/
+
+  public menuCategorias = false;
+  public menuEspecialidades = false;
+  public menuUbicaciones = false;
+  public menuOtros = false;
+
+  public subcatEquipos = false;
+  public subcatHerramientas = false;
+  public subcatAccesorios = false;
+  public subcatMotores = false;
+
+
+  constructor(private renderer:Renderer2) { }
 
   ngOnInit(): void {
     this.categoriaEquipos = true;
   }
+
+  leaveelement(dato:string){
+    this.renderer.removeClass(this.categoriaM.nativeElement, "clasePrueba");
+    switch(dato){
+      case 'categorias':
+        this.renderer.removeClass(this.categoriaM.nativeElement, "clasePrueba");
+      break;
+      case 'especialidades':
+        this.renderer.removeClass(this.especialidadesM.nativeElement, "clasePrueba");
+      break;
+      case 'ubicaciones':
+        this.renderer.removeClass(this.ubicacionM.nativeElement, "clasePrueba");
+      break;
+      case 'otros':
+        this.renderer.removeClass(this.otrosM.nativeElement, "clasePrueba");
+      break;
+    }
+  }
+
+
+  expandSubcategorias(dato){
+    switch(dato){
+      case 'equipos':
+        console.log('son equipos');
+        this.renderer.addClass(this.contenedorP.nativeElement, "contenedorPruebaExpand");
+        this.subcatEquipos = true;
+        this.subcatAccesorios = false;
+        this.subcatHerramientas = false;
+        this.subcatMotores = false;
+       
+      break;
+      case 'herramientas':
+        console.log('Son herramientas');
+        this.renderer.addClass(this.contenedorP.nativeElement, "contenedorPruebaExpand");
+        this.subcatEquipos = false;
+        this.subcatAccesorios = false;
+        this.subcatHerramientas = true;
+        this.subcatMotores = false;
+      break;
+      case 'accesorios':
+        console.log('Son accesorios');
+        this.renderer.addClass(this.contenedorP.nativeElement, "contenedorPruebaExpand");
+        this.subcatEquipos = false;
+        this.subcatAccesorios = true;
+        this.subcatHerramientas = false;
+        this.subcatMotores = false;
+      break;
+      case 'motores':
+        console.log('Son motorres');
+        this.renderer.addClass(this.contenedorP.nativeElement, "contenedorPruebaExpand");
+        this.subcatEquipos = false;
+        this.subcatAccesorios = false;
+        this.subcatHerramientas = false;
+        this.subcatMotores = true;
+      break;
+    }
+  }
+
+
+  minimizarSubMenu(){
+    this.renderer.removeClass(this.contenedorP.nativeElement, "contenedorPruebaExpand");
+  }
+  
+
+  selectCategory(valor:string){
+    console.log("Supp bitches", valor);
+    switch(valor){
+      case 'herramientas': 
+        console.log('Es una herramienta');
+        this.categoriaEquipos = false;
+        this.categoriaHerramientas = true;
+        this.categoriaAccesorios = false;
+        this.categoriaMotores = false;
+      break;
+      case 'equipos':
+        console.log('Es un equipo');
+        this.categoriaEquipos = true;
+        this.categoriaHerramientas = false;
+        this.categoriaAccesorios = false;
+        this.categoriaMotores = false;
+      break;
+      case 'accesorios':
+        console.log('Son accesorios');
+        this.categoriaEquipos = false;
+        this.categoriaHerramientas = false;
+        this.categoriaAccesorios = true;
+        this.categoriaMotores = false;
+      break;
+      case 'motores':
+        console.log('Son motores');
+        this.categoriaEquipos = false;
+        this.categoriaHerramientas = false;
+        this.categoriaAccesorios = false;
+        this.categoriaMotores = true;
+      break;
+      
+    
+  }
+
+} 
+
+  expandMenu(){
+    console.log('This is the fucking width',this.primerVista.nativeElement.offsetWidth);
+    let widthElement = this.primerVista.nativeElement.offsetWidth;
+    if(widthElement == 0){
+      this.renderer.addClass(this.primerVista.nativeElement, "expandfirst");
+    }else{
+      this.renderer.removeClass(this.primerVista.nativeElement, "expandfirst");
+    }
+    
+  }
+
+  expandCategories(dato){
+    switch(dato){
+      case 'categorias':
+        let opcCategoryHeight = this.opcCategorias.nativeElement.offsetHeight;
+        console.log(opcCategoryHeight)
+        if(opcCategoryHeight == 0){
+          this.renderer.addClass(this.opcCategorias.nativeElement, "expandCm");
+        }else{
+          this.renderer.removeClass(this.opcCategorias.nativeElement, "expandCm");
+        }
+      break;
+      case 'ubicaciones':
+        let opcUbicacionHeight = this.opcUbicacion.nativeElement.offsetHeight;
+        if(opcUbicacionHeight == 0){
+          this.renderer.addClass(this.opcUbicacion.nativeElement, "expandCm");
+        }else{
+          this.renderer.removeClass(this.opcUbicacion.nativeElement, "expandCm");
+        }
+      break;
+      case 'especialidades':
+        let opcEspecialidades = this.opcEspecialidad.nativeElement.offsetHeight;
+        if(opcEspecialidades == 0){
+          this.renderer.addClass(this.opcEspecialidad.nativeElement, "expandCm");
+        }else{
+          this.renderer.removeClass(this.opcEspecialidad.nativeElement, "expandCm");
+        }
+      break;
+    }
+    
+  } 
+
+
+  selectMenu(valor:string){
+    switch(valor){
+      case 'categorias':
+        this.renderer.addClass(this.categoriaM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.especialidadesM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.ubicacionM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.otrosM.nativeElement, "clasePrueba");
+      break;
+      case 'especialidades': 
+        this.renderer.removeClass(this.categoriaM.nativeElement, "clasePrueba");
+        this.renderer.addClass(this.especialidadesM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.ubicacionM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.otrosM.nativeElement, "clasePrueba");
+       
+      break;
+      case 'ubicaciones': 
+        this.renderer.removeClass(this.categoriaM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.especialidadesM.nativeElement, "clasePrueba");
+        this.renderer.addClass(this.ubicacionM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.otrosM.nativeElement, "clasePrueba");
+      
+      break;
+      case 'otros': 
+        this.renderer.removeClass(this.categoriaM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.especialidadesM.nativeElement, "clasePrueba");
+        this.renderer.removeClass(this.ubicacionM.nativeElement, "clasePrueba");
+        this.renderer.addClass(this.otrosM.nativeElement, "clasePrueba");
+      break;
+
+
+    }
+  }
+
+
 
 }
