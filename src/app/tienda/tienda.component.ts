1d3dc509b0b3c03ab9ce40205da4df1e5b28cdc5
fecego.adrian <fecego.git@gmail.com>
2021-09-10 17:44:46 -0500

    Actualizacion numero 1

--- src/app/tienda/tienda.component.ts
+++ src/app/tienda/tienda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tienda',
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tienda.component.css']
 })
 export class TiendaComponent implements OnInit {
+  @ViewChild('filtrosResponsive') filResponsive:ElementRef;
+  @ViewChild('botonResponsiveFiltros') botonResponsiveF: ElementRef;
+  @ViewChild('filtrosRExpand') filtrosRexpand:ElementRef;
+  constructor(private render:Renderer2) { 
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+  }
+
+  openFiltrosResponsive(){
+      const medidaFilterResponsive = this.filResponsive.nativeElement.offsetWidth;
+      console.log('Presionaste openFiltros')
+      if(medidaFilterResponsive == 0){
+        this.render.addClass(this.filResponsive.nativeElement, 'clasificacion-responsivaExpand');
+        this.render.addClass(this.botonResponsiveF.nativeElement, 'indexIncrease');
+      }else{
+        this.render.removeClass(this.filResponsive.nativeElement, 'clasificacion-responsivaExpand');
+        this.render.removeClass(this.botonResponsiveF.nativeElement, 'indexIncrease');
+
+      }
+
+      
+  }
+
+  expandFiltros(){
+    const mR = this.filtrosRexpand.nativeElement.offsetHeight;
+    if(mR == 0){
+      this.render.addClass(this.filtrosRexpand.nativeElement, 'filtrosExpExpand')
+    }else{
+      this.render.removeClass(this.filtrosRexpand.nativeElement, 'filtrosExpExpand');
+    }
+    
   }
 
 }
